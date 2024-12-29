1d5f9136dd2bf3ba64703dd779025ab2e1782832
Elisabeth Klassen <erkuvic@gmail.com>
2024-12-28 17:38:13 -0800

    initial commit

---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+export default function Header () {
+
+    return (
+        <div className='header-background'>
+            <h1>
+                Michael Klassen
+            </h1>
+        </div>
+    )
+}
