0847a4be17278226203c8dcd59adb2360feea936
Elisabeth Klassen <erkuvic@gmail.com>
2024-12-28 18:09:56 -0800

    adding about me section

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,10 +3,20 @@ import './Header.css'
 export default function Header () {
 
     return (
-        <div className='header-background'>
-            <h1>
-                Michael Klassen
-            </h1>
+        <div className='main-page-container'>
+            <div className='header-background'>
+                <h1>
+                    Michael Klassen
+                </h1>
+            </div>
+            <div className='about-me-background'>
+                <h2>
+                    About Me
+                </h2>
+                <p>
+                    Lorem ipsum dolor
+                </p>
+            </div>
         </div>
     )
 }
