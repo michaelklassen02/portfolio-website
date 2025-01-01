3665efd9ed839be3d283bb2d6b2041c961ca4a09
Elisabeth Klassen <erkuvic@gmail.com>
2025-01-01 15:34:19 -0800

    About Me page

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,7 +7,16 @@ export default function AboutMe () {
                 About Me
             </h2>
             <p>
-                Lorem ipsum dolor
+                I am a graduate from the University of Victoria with a Bachelor's Degree in Software Engineering, and I am now looking for my first long-term job in software engineering. I'm interested in a variety of fields, but my specialty is in web development. I have a lot of practice in frontend web design and would love to help design new and compelling websites for your company. 
+            </p>
+            <p>
+                I'm skilled in a variety of frameworks and programming languages, including React, TypeScript, and CSS, which this website is created in. For more on my qualifications, you can visit the Qualifications page. To see some examples of my work, visit the Technical Demos page.
+            </p>
+            <p>
+                I have co-op experience with multiple companies that would bring me an edge working at your company, including two QA positions and two web developer positions. For more details, visit the Work Experience page. 
+            </p>
+            <p>
+                I look forward to hearing from you! Please feel free to reach out to me from the Contact page if you have any questions or are interested in any of my skills.
             </p>
         </div>
     )
