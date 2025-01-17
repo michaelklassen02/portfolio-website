f7e7a9fecacb669c62b53edf6c4be01a90aa88fc
Elisabeth Klassen <erkuvic@gmail.com>
2025-01-17 15:14:16 -0800

    chart detailing

--- src/components/GraphDemo.tsx
+++ src/components/GraphDemo.tsx
@@ -1,5 +1,6 @@
 import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 import { graphDemoData } from "../constants/chartdata";
+import '../styles/Modal.css'
 
 export default function GraphDemo () {
     const data = graphDemoData;
@@ -8,12 +9,17 @@ export default function GraphDemo () {
     )
 
     return (
-        <LineChart width={800} height={400} data={graphDemoData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-            <Line type='monotone' dataKey='y' stroke='#8884d8' />
-            <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
-            <XAxis dataKey='x' type='number'/>
-            <YAxis />
-            <Tooltip />
-        </LineChart>
+        <div>
+            <LineChart width={800} height={400} data={data} margin={{ top: 10, right: 20, bottom: 5, left: 0 }}>
+                <Line type='monotone' dataKey='y' stroke='#8884d8' />
+                <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
+                <XAxis dataKey='x' type='number'/>
+                <YAxis />
+                <Tooltip />
+            </LineChart>
+            <p className='job-description'>
+                This is a sample chart to illustrate the process of making a chart with arbitrary data. This chart was made using the Recharts library for React.
+            </p>
+        </div>
     )
 }
