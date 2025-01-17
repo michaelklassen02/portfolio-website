f0aa6b7eaea1e7679d6ef41e219efc98ea2387dc
Elisabeth Klassen <erkuvic@gmail.com>
2025-01-17 14:06:36 -0800

    reorganizing axis

--- src/components/GraphDemo.tsx
+++ src/components/GraphDemo.tsx
@@ -2,11 +2,16 @@ import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts"
 import { graphDemoData } from "../constants/chartdata";
 
 export default function GraphDemo () {
+    const data = graphDemoData;
+    data.sort((a, b) => 
+        a.x > b.x ? 1 : -1
+    )
+
     return (
         <LineChart width={800} height={400} data={graphDemoData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
             <Line type='monotone' dataKey='y' stroke='#8884d8' />
             <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
-            <XAxis dataKey='x' />
+            <XAxis dataKey='x' type='number'/>
             <YAxis />
             <Tooltip />
         </LineChart>
